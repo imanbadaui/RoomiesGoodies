844dcc000ea7538ce9adc63ccd6652d7d3755918
Iman Albadaui <iman.badaui@gmail.com>
2024-10-21 13:54:04 +0300

    search feature added and table of all products

--- back/app.py
+++ back/app.py
@@ -3,6 +3,7 @@ import json
 from flask_cors import CORS
 import hashlib
 from user import User
+from product import Product
 from data_api import read_json_db , write_json_db
 
 app = flask.Flask("RoomiesGoodies") #creates web App
@@ -12,13 +13,15 @@ CORS(app)
 def hash_pass(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
+
 # Specify the path to the JSON file
 users_db = 'user_auth_db.json'
 products_db = 'products_db.json'
 
-#change from static to dynamic. front end must send first admin details.
+
+#Future change: convert from hardcoding to dynamic. front end must send first admin details.
 #create first admin
-#loli password = 123
+#Note: in users_db, All passwords other than "Immz" is = 123
 def create_first_admin():
 	first_admin_name = "Immz"
 	first_admin_pass = "3e37844873"
@@ -28,11 +31,15 @@ def create_first_admin():
 	first_user_dict = first_user.to_dict()
 	write_json_db(users_db, first_user_dict)
 
-#create first admin only if not exist
+#create first admin only if not exist in DB.
 data = read_json_db(users_db)
 if not data:
 	create_first_admin()
 
+
+
+
+##### Login API #####
 #CRUD READ
 #receive user login data from front end
 @app.route("/login" , methods=['POST'])
@@ -43,7 +50,6 @@ def receive_login_data():
 	data = flask.request.get_json()  
 	username = data.get("username")
 	password = data.get("password")
-	print(password)
 	#hash received password to compare with stored hashed password
 	hashed_pass = hash_pass(password)
 
@@ -64,25 +70,41 @@ def receive_login_data():
 	return flask.jsonify({'message': message , 'user_is_Admin' : user_is_admin})  # Respond with a message
 
 
-#CRUD CREATE
-#receive granted user data from front end
+
+
+
+##### Admin API #####
+#CRUD CREATE.
+#receives granted user data from front end
 @app.route("/newUserGrant" , methods=['POST'])
 def receive_newuser_grant_data():
 	# Get the JSON newuser grant sent from the frontend
 	data = flask.request.get_json()  
 	new_username = data.get("newUsername")
 	is_new_user_admin = data.get("isNewUserAdmin")
+	user_exist = False
 	if new_username != "":
-		new_user = User(new_username, "", is_new_user_admin)
-		new_user_dict = new_user.to_dict()
-		write_json_db(users_db, new_user_dict)
-		#1 means created successfully
-		return flask.jsonify({'message': "1"})
-	else:
-		#0 means there was no username sent and no object created in DB
-		return flask.jsonify({'message': "0"})
+		allusers_data = read_json_db(users_db)
+		for user in allusers_data:
+			if user['username'] == new_username:
+				user_exist = True
+				break
+		if not user_exist:
+			new_user = User(new_username, "", is_new_user_admin)
+			new_user_dict = new_user.to_dict()
+			write_json_db(users_db, new_user_dict)
+			#1 means created successfully
+			return flask.jsonify({'message': "1"})
+		else:
+		#0 means user already exists
+			return flask.jsonify({'message': "0"})
 
 
+
+
+
+##### Forgot Password API #####
+#CRUD UPDATE.
 #CRUD UPDATE
 #receive password of granted user data from front end
 @app.route("/newAccount" , methods=['POST'])
@@ -92,7 +114,6 @@ def receive_newAccount_password_data():
 	granted_username = data.get("grantedUsername")
 	new_password = data.get("newPassword")
 
-	data_validated = False
 	user_exists = False
 
 	if granted_username != "" and new_password != "":
@@ -101,30 +122,23 @@ def receive_newAccount_password_data():
 		hashed_pass = hash_pass(new_password)
 		#if user with password already exist
 		for user in allusers_data:
-			if user['username'] == granted_username and user['password'] != "":
-				#2 implies user already exists
-				user_exists= True
-				break
-			elif user['username'] == granted_username: # Check if the username exist in DB
-				if user['password'] == "": #avoid re-update password
+			if user['username'] == granted_username:
+				if user['password'] != "":
+					user_exists= True
+					break
+				else:
 					user['password'] = hashed_pass
 					#convert JSON list of objects to string
 					ALL_data_str = json.dumps(allusers_data)
 					delete_record_helper(users_db, ALL_data_str)
-					data_validated = True
-					break
-				else: 
-					user_exists= True
-
+					user_exists = False	
+					break	
 		if user_exists:
-			return flask.jsonify({'message': "2"})
+			#-1 implies user already exists
+			return flask.jsonify({'message': "-1"})
 		else:
-			if data_validated:
-				#1 implies data stored successfully
+			#1 implies data stored successfully            
 				return flask.jsonify({'message': "1"})
-			else: 
-				#-1 implies user is not found
-				return flask.jsonify({'message': "-1"})
 	else:
 		#0 means an empty data is sent
 		return flask.jsonify({'message': "0"})
@@ -136,4 +150,106 @@ def delete_record_helper(file_path, data_str):
 	file.truncate(0)
 	#data_str.replace('},', '},\n')
 	file.write(data_str)
-	file.close()
+
+
+
+
+
+
+##### Forgot Password API #####
+#CRUD UPDATE.
+#receives granted user and new password.
+@app.route("/newPassword" , methods=['POST'])
+def receive_new_password_data():
+	#Get the JSON password and granted username sent from the frontend
+	data = flask.request.get_json()  
+	granted_username = data.get("grantedUsername")
+	new_password = data.get("newPassword")
+	user_exists = False
+
+	if granted_username != "" and new_password != "":
+		#read users from JSON
+		allusers_data = read_json_db(users_db)
+		hashed_pass = hash_pass(new_password)
+		
+		for user in allusers_data:
+			#if user with password already exist
+			if user['username'] == granted_username:
+				if user['password'] != "":
+					#remove old password
+					user['password'] = ""
+					#add new password
+					user['password'] = hashed_pass
+					#convert JSON list of objects to string
+					ALL_data_str = json.dumps(allusers_data)
+					delete_record_helper(users_db, ALL_data_str)
+					user_exists = True
+					break
+
+		if user_exists:
+			#1 implies data stored successfully            
+			return flask.jsonify({'message': "1"})
+		else:
+			#-1 implies user doesn't have an account yet to use forgot password
+			return flask.jsonify({'message': "-1"})
+	else:
+		#0 means an empty data is sent
+		return flask.jsonify({'message': "0"})
+
+
+
+
+
+##### Reading All products API #####
+#CRUD READ.
+@app.route("/readProducts" , methods=['POST'])
+def allproducts_request():
+	allproducts_data = read_json_db(products_db)
+
+	#list of all the products to be sent to front end
+	products_list = []
+
+	for product in allproducts_data:
+		selected_product = Product(product['code'], product['name'],product['owner'], product['price'], product['type'] ,product['quantity'],  product['unit'], product['access'])
+		products_list.append(selected_product)
+
+	serialized_products = [product.to_dict() for product in products_list]
+	return flask.jsonify(serialized_products)
+
+
+
+
+
+##### Searching API #####
+#CRUD UPDATE.
+#searching products by product name or owner name.
+@app.route("/query" , methods=['POST'])
+def receive_search_query():
+	query = flask.request.get_json()  
+	search_type = query.get("check_type")
+	search_value = query.get("query_value")
+	allproducts_data = read_json_db(products_db)
+
+	#list of all the selected products to be sent to front end
+	sent_products_list = []
+
+	if search_type == "product":
+		#If product, loop over all products and add selected to list
+		for prod in allproducts_data:
+			selected_product = Product(prod['code'], prod['name'],prod['owner'], prod['price'], prod['type'] ,prod['quantity'],  prod['unit'], prod['access'])
+			#return all products with this product name
+			if selected_product.product_name.lower() == search_value.lower():
+				sent_products_list.append(selected_product)
+
+	elif search_type == "owner":
+		#If owner, loop over all products and add products belong to this owner to list.
+		for prod in allproducts_data:
+			selected_product = Product(prod["code"], prod["name"],prod["owner"], prod["price"], prod["type"] ,prod["quantity"],  prod["unit"], prod["access"])
+			#return all products with this product name
+			if selected_product.product_owner.lower() == search_value.lower():
+				sent_products_list.append(selected_product)
+
+	serialized_products = [product.to_dict() for product in sent_products_list]
+	return flask.jsonify(serialized_products)
+
+
--- back/product.py
+++ back/product.py
@@ -1,82 +1,33 @@
-import User
-#class to list all the products info at one's home.
+#class to list all the products info at home.
 class Product:
-    __product_name = ""
-    __product_owner = ""
-    #quantity and unit
-    __quantity_tuple = (0, "")
-    __product_code = 0
-    __product_price = 0.0
-    __is_shared = False
+    product_code = ""
+    product_name = ""
+    product_owner = ""
+    product_price = 0.0
+    product_quantity = 0.0
+    product_type = ""
+    product_unit = ""
+    product_access = 0 #personal 1, shared 2
    
-    def __init__(self, product_name, quantity_tuple, product_code, product_price, is_shared):
-        self.__product_name = product_name
-        self.__quantity_tuple = quantity_tuple
-        self.__product_code = product_code
-        self.__product_price = product_price
-        self.__is_shared = is_shared
-        #once a new product added,it will auto add the owner name as the username.
-        #only owner of product can add it.
-        self.__product_owner = User.__username 
-       
+   #receives the username to be stored as the owner name.
+    def __init__(self, product_code, product_name,productOwnerUsername, product_price, product_type, product_quantity, product_unit, product_access):
+        self.product_code = product_code
+        self.product_name = product_name
+        self.product_owner = productOwnerUsername
+        self.product_price = product_price
+        self.product_type = product_type
+        self.product_quantity = product_quantity
+        self.product_unit = product_unit
+        self.product_access = product_access 
 
-    #only owner of a product can change ownership of that product.
-    @__product_owner.setter
-    def set_owner(self, new_owner):
-        if self.__product_owner == User.__username:
-            self.__product_owner = new_owner
-
-    #only owner of a product can change name of that product.
-    @__product_name.setter
-    def set_name(self, new_name):
-        if self.__product_owner == User.__username:
-            self.__product_name = new_name
-
-    #only owner of a product can change quantity of that product.
-    @__product_owner.setter
-    def set_quantity(self, new_quantity):
-        if self.__product_owner == User.__username:
-            self.__quantity_tuple = new_quantity
-    
-    #only owner of a product can change code of that product.
-    @__product_code.setter
-    def set_code(self, new_code):
-        if self.__product_owner == User.__username:
-            self.__product_code = new_code
-
-    #only owner of a product can change price of that product.
-    @__product_owner.setter
-    def set_price(self, new_price):
-        if self.__product_owner == User.__username:
-            self.__product_price = new_price
-
-    #only owner of a product can change access of that product.
-    @__is_shared.setter
-    def set_is_shared(self, new_access):
-        if self.__product_owner == User.__username:
-            self.__is_shared = new_access
-            
-
-    @property
-    def get_name(self):
-            return self.__product_name
-    
-    @property
-    def get_owner(self):
-        return self.__product_owner
-    
-    @property
-    def get_code(self):
-        return self.__product_code
-    
-    @property
-    def get_price(self):
-        return self.__product_price
-    
-    @property
-    def get_quantity(self):
-        return self.__quantity_tuple
-    
-    @property
-    def get_access(self):
-        return self.__is_shared
+    def to_dict(self):
+        return {
+            'code':  self.product_code,
+            'name': self.product_name,
+            'owner': self.product_owner,
+            'price': self.product_price,
+            'type': self.product_type,
+            'quantity': self.product_quantity,
+            'unit':  self.product_unit,
+           'access' : self.product_access
+        }
